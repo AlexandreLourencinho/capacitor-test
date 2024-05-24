fcc6c29e2ff59f5d01560221ada2a7f778d726e6
Alexandre LOURENCINHO <alexandre.lourencinho@cgi.com>
2024-05-24 18:09:59 +0200

    [FEAT] downgrade attempt

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.capacitortest',
   appName: 'capacitor-test',
-  webDir: 'dist/capacitor-test/browser'
+  webDir: 'dist/capacitor-test/'
 };
 
 export default config;

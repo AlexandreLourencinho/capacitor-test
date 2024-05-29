0c26d04d0e78b8a0d35881f0d7efb6800e037196
Alexandre <alexandre.lourencinho@gmail.com>
2024-05-29 22:06:38 +0200

    [feat] addition of Room

--- android/app/src/main/java/com/example/capacitortest/MainActivity.java
+++ android/app/src/main/java/com/example/capacitortest/MainActivity.java
@@ -2,4 +2,7 @@ package com.example.capacitortest;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+//  https://developer.android.com/training/data-storage/room?hl=fr
+
+}
